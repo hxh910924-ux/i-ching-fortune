1fba2826b921e0dd751ede8b551ea92ee1c6fae6
hxh910924-ux <hxh910924@gmail.com>
2026-06-24 15:48:32 +0800

    feat: 9大功能增强 - 变卦/爻辞/纳甲/铜钱动画/PWA/分享/每日卦/收藏对比/手动摇卦

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Service Worker for 周易六爻
+const CACHE_NAME = 'iching-v2';
+const urlsToCache = [
+    './',
+    'index.html',
+    'manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(names => Promise.all(
+            names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+        ))
+    );
+});
